551eb93038aba24fd9a900f02a5f88f074f24b07
ben <ben@ksbdental.com>
2020-03-02 16:00:56 -0600

    copied to /docs folder for demo

--- docs/stopwatch.js
+++ docs/stopwatch.js
@@ -1,37 +1,46 @@
 class Timestamp {
-    constructor(time, action){
+    constructor(time, action) {
         this.time = time;
         this.action = action;
     }
 }
 
-let tenths = 0;
+const startBtn = document.getElementById('startBtn');
+const stopBtn = document.getElementById('stopBtn');
+const lapBtn = document.getElementById('lapBtn');
+const timeEl = document.getElementById('time-string');
+const lapsEl = document.getElementById('lap-history');
+
+let tenths = 1;
 let timer;
 let isRunning = false;
 let tsArr = [];
+let elapsedTime = 0;
+let blurTS;
 
 let s = 0;
 let m = 0;
 let h = 0;
 
 function onStartClick() {
-    tsArr.push(new Timestamp(Date.now(), 'START'));
     timer = setInterval(increment, 100);
+    tsArr.push(new Timestamp(Date.now(), 'START'));
     isRunning = true;
-    document.getElementById('startBtn').setAttribute('disabled', 'true');
-    document.getElementById('stopBtn').removeAttribute('disabled');
-    document.getElementById('lapBtn').removeAttribute('disabled');
+    startBtn.setAttribute('disabled', 'true');
+    stopBtn.removeAttribute('disabled');
+    lapBtn.removeAttribute('disabled');
 }
 
 function onStopClick() {
     tsArr.push(new Timestamp(Date.now(), 'STOP'));
-    if (timer) {
-        clearInterval(timer);
-        isRunning = false;
-        document.getElementById('startBtn').removeAttribute('disabled');
-        document.getElementById('stopBtn').setAttribute('disabled', 'true');
-        updateLapsDisplay();
-    }
+    clearInterval(timer);
+    // if (timer) {
+    // clearInterval(timer);
+    isRunning = false;
+    startBtn.removeAttribute('disabled');
+    stopBtn.setAttribute('disabled', 'true');
+    updateLapsDisplay();
+    // }
 }
 
 function onLapClick() {
@@ -41,11 +50,12 @@ function onLapClick() {
         m = 0;
         h = 0;
         tsArr = [];
-        elapsed = 0;
-        document.getElementById('startBtn').removeAttribute('disabled');
-        document.getElementById('lapBtn').setAttribute('disabled', 'true');
-        document.getElementById('stopBtn').setAttribute('disabled', 'true');
+        elapsedTime = 0;
+        startBtn.removeAttribute('disabled');
+        lapBtn.setAttribute('disabled', 'true');
+        stopBtn.setAttribute('disabled', 'true');
         updateTimerDisplay();
+        tenths = 1;
     } else {
         tsArr.push(new Timestamp(Date.now(), 'LAP'));
 
@@ -54,8 +64,8 @@ function onLapClick() {
 }
 
 function increment() {
-    tenths++;
     updateTimerDisplay();
+    tenths++;
 }
 
 function numToString(num) {
@@ -77,8 +87,9 @@ function msToString(milis) {
 }
 
 function updateTimerDisplay() {
-    let timeEl = document.getElementById('time-string');
-    timeEl.innerText = convertToTimeString(/*tenths*/);
+    // let timeEl = document.getElementById('time-string');
+    // timeEl.innerText = convertToTimeString(/*tenths*/);
+    timeEl.textContent = convertToTimeString();
 }
 
 function convertToTimeString(/*tenthsToBeConverted*/) {
@@ -99,11 +110,12 @@ function convertToTimeString(/*tenthsToBeConverted*/) {
         s = 0;
         m++;
     }
-    if (m === 60){
+    if (m === 60) {
         m = 0;
         h++;
     }
-    return `${numToString(h)}:${numToString(m)}:${numToString(s)}.${tenths}`;
+    // return `${h.toString().padStart(2,'0')}:${m.toString().padStart(2, '0')}:${s.toString().padStart(2, '0')}.${tenths}`;
+    return h.toString().padStart(2, '0') + ':' + m.toString().padStart(2, '0') + ':' + s.toString().padStart(2, '0') + '.' + tenths;
 }
 
 function convertMsToObject(msToConvert) {
@@ -118,7 +130,7 @@ function convertMsToObject(msToConvert) {
 }
 
 function updateLapsDisplay() {
-    let lapsEl = document.getElementById('lap-history');
+    // let lapsEl = document.getElementById('lap-history');
     lapsEl.innerHTML = "";
     let elapsed = 0;
     let lapCount = 0;
@@ -137,17 +149,77 @@ function updateLapsDisplay() {
             elapsed += diff;
         }
 
-        if (currentAct !== 'START'){
+        if (currentAct !== 'START') {
             lapCount++;
         }
 
-        console.log(elapsed);
+        // console.log(elapsed);
         let diffObj = convertMsToObject(diff);
         let elapObj = convertMsToObject(elapsed);
-        console.log(elapObj);
+        // console.log(elapObj);
         if (i > 0 && currentAct !== 'START') {
-            lapsEl.innerHTML += `<p>Lap ${numToString(lapCount)} - ${numToString(diffObj.hours)}:${numToString(diffObj.minutes)}:${numToString(diffObj.seconds)}.${msToString(diffObj.miliseconds)} - ${numToString(elapObj.hours)}:${numToString(elapObj.minutes)}:${numToString(elapObj.seconds)}.${msToString(elapObj.miliseconds)}</p>`;
+            // lapsEl.innerHTML += `<p>Lap ${numToString(lapCount)} - ${numToString(diffObj.hours)}:${numToString(diffObj.minutes)}:${numToString(diffObj.seconds)}.${msToString(diffObj.miliseconds)} - ${numToString(elapObj.hours)}:${numToString(elapObj.minutes)}:${numToString(elapObj.seconds)}.${msToString(elapObj.miliseconds)}</p>`;
+            lapsEl.innerHTML += `<p>Lap ${lapCount.toString().padStart(2, '0')} - ${numToString(diffObj.hours)}:${numToString(diffObj.minutes)}:${numToString(diffObj.seconds)}.${msToString(diffObj.miliseconds)} - ${numToString(elapObj.hours)}:${numToString(elapObj.minutes)}:${numToString(elapObj.seconds)}.${msToString(elapObj.miliseconds)}</p>`;
         }
-        
+
     }
+}
+
+// function updateLapsDisplay2() {
+//     let newLapText = "";
+//     let newLapPara = document.createElement('p');
+//     let lastTSIndex = tsArr.length - 1;
+//     let prevTSIndex = tsArr.length - 2 < 0 ? 0 : tsArr.length - 2;
+//     let lastTS = tsArr[lastTSIndex];
+//     let prevTS = tsArr[prevTSIndex];
+
+//     diff = lastTS.time - prevTS.time;
+//     if (prevTS.action !== 'STOP') {
+//         elapsedTime += diff;
+//     }
+//     let lapCount = tsArr.filter(ts => ts.action !== 'START').length;
+//     let diffObj = convertMsToObject(diff);
+//     let elapObj = convertMsToObject(elapsedTime);
+//     if (lastTS.action !== 'START') {
+//         newLapText = `Lap ${lapCount.toString().padStart(2,'0')} - ${diffObj.hours.toString().padStart(2, '0')}:${diffObj.minutes.toString().padStart(2, '0')}:${diffObj.seconds.toString().padStart(2, '0')}.${diffObj.miliseconds.toString().padStart(3, '0')} - ${elapObj.hours.toString().padStart(2, '0')}:${elapObj.minutes.toString().padStart(2, '0')}:${elapObj.seconds.toString().padStart(2, '0')}.${msToString(elapObj.miliseconds)}`;
+//         newLapPara.textContent = newLapText;
+//         lapsEl.appendChild(newLapPara);
+//     }
+
+
+// }
+
+window.onblur = (event) => {
+    if (timer && isRunning) {
+        blurTS = new Timestamp(Date.now(), 'BLUR');
+        clearInterval(timer);
+    }
+    // console.log(event);
+}
+
+window.onfocus = (event) => {
+    let nowms = Date.now();
+    // console.log(event);
+    // console.log(blurTS);
+    if (blurTS) {
+        // determine difference between now and blurTS
+        let difftime = nowms - blurTS.time;
+        // console.log(difftime);
+        // add difference to existing amount of time that passed
+        // first, convert displayed time-passed to milliseconds
+        let currentMS = (tenths * 100) + (s * 1000) + (m * 60000) + (h * 3600000);
+        // console.log(currentMS);
+        let newMS = difftime + currentMS;
+        // console.log(newMS);
+        // then update the displayed times
+        let currentDisplayTimeObj = convertMsToObject(newMS);
+        // console.log(currentDisplayTimeObj);
+        h = currentDisplayTimeObj.hours;
+        m = currentDisplayTimeObj.minutes;
+        s = currentDisplayTimeObj.seconds;
+        tenths = Math.floor(currentDisplayTimeObj.miliseconds / 100) + 1;//add one because interval executes after 100ms
+        // restart interval, which will update display
+        timer = setInterval(increment, 100);
+    }
+    blurTS = null;
 }
